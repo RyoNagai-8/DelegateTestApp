716e630a1c08f0bf11fb7744cb13dff008bd337b
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-06-06 10:25:29 +0900

    Add Navigation Controller

--- DelegateTestApp/ViewController.swift
+++ DelegateTestApp/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let cellId = "cellId"
 
+    @IBOutlet weak var testTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
+        testTableView.delegate = self
+        testTableView.dataSource = self
+        //線が消える。
+        testTableView.tableFooterView = UIView()
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = testTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    
 }
 
